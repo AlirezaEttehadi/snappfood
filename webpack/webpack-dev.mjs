be66a3a9a9449d1d3cca57badc0e52d040dad8dc
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-04-29 22:15:44 +0330

    chore: add react, babel and typescript

--- webpack/webpack-dev.mjs
+++ webpack/webpack-dev.mjs
@@ -10,12 +10,54 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 });
 
 export default {
+  mode: "development",
   context: __dirname,
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].[contenthash].js",
     publicPath: "/",
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    client: {
+      reconnect: 5,
+      overlay: true,
+      progress: true,
+      logging: "none",
+    },
+    port: 3000,
+    open: false,
+    compress: false,
+    historyApiFallback: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheCompression: false,
+            cacheDirectory: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: "ts-loader",
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
+  },
 };
